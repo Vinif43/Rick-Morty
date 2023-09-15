fcb1cec3701ff956ce49a838f06d8a4ca9854472
vinif <vinifmarinhoo@gmail.com>
2023-09-15 02:10:37 -0300

    test tree

--- src/app/components/Character/Card.tsx
+++ src/app/components/Character/Card.tsx
@@ -19,6 +19,7 @@ export default function Card() {
         return (
           <div key={item.id}>
             <h1>{item.name}</h1>
+
             <img src={item.image} alt={item.name} />
           </div>
         )
