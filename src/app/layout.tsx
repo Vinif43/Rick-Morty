777b872b7291405ac084d28df33af1a04b25e4c6
vinif <vinifmarinhoo@gmail.com>
2023-09-15 02:01:32 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
 import ReactQueryProvider from './components/ReactQueryProvider'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
